ff0ddb6da63c5eb4be306bb07e1c1edacd21fd23
Slovak_Cat <slovak_cat@skcat.tk>
2020-11-20 10:42:27 +0100

    Update meow.js

--- meow.js
+++ meow.js
@@ -85,28 +85,3 @@ function w_Che(a, b) {
  }
 
 
-/**
- * Dubiously created by Adrian Cooney
- * http://adriancooney.github.io
- */
-(function(console) {
-	"use strict";
-
-	function getBox(width, height) {
-		return {
-			string: "+",
-			style: "font-size: 1px; padding: " + Math.floor(height/2) + "px " + Math.floor(width/2) + "px; line-height: " + height + "px;"
-		}
-	}
-	console.image = function(url, scale) {
-		scale = scale || 1;
-		var img = new Image();
-
-		img.onload = function() {
-			var dim = getBox(this.width * scale, this.height * scale);
-			console.log("%c" + dim.string, dim.style + "background: url(" + url + "); background-size: " + (this.width * scale) + "px " + (this.height * scale) + "px; color: transparent;");
-		};
-
-		img.src = url;
-	};
-})(console);
